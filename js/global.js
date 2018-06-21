29972c325bbbb46b0d0e075f543d141a77572dda
html546 <1776787598@qq.com>
2018-06-21 14:14:31 +0800

    333

--- js/global.js
+++ js/global.js
@@ -1,1 +1,6 @@
 var BaseUrl = 'http://yimall.host5.liuniukeji.com';
+function GetQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]); return null;
+}  
